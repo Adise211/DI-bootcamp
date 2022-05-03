ae2f65e3d0f17174d9dbbf35cf84a523b10bfcca
Adise211 <adisemamo211@gemail.com>
2022-05-03 22:34:06 +0300

    exercise d1

---
+++ w7/d1/npm-node/main.js
@@ -0,0 +1,18 @@
+// Exercise 1:
+let a = 5;
+let b = 10;
+
+const sum = a + b;
+console.log(`Hello you are ${sum}`);
+
+// Exercise 2:
+console.log('Hi, thanks for running me with npm start!');
+
+// Exrcise 3:
+
+let x;
+let c = 6;
+x = a + b;
+
+console.log(`first run: ${x}`);
+console.log(`second run: ${c + x}`);
